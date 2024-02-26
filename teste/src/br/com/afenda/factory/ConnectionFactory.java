83b490f60b18a603743c64bc958412e641bff335
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2024-02-26 11:08:37 -0300

    Add files via upload

---
+++ teste/src/br/com/afenda/factory/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package br.com.afenda.factory;
+
+public class ConnectionFactory {
+
+	//nome do usuário do mysql
+	private static final String USARNAME = "root";
+	
+	//Senha do banco
+	private static final String PASSWORD = "";
+	
+	
+	//Caminho do banco de dados, porta, nome do banco de dados
+	private static final String DATABASE_URL = "jdbc:mysql://localhsot:3306/agenda";
+	
+	/*
+	 * 
+	 * Conexão com banco de dados
+	 */
+	public static Connection creatConnectionTollySQL() {
+		class
+	}
+}
