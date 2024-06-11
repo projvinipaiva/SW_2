607fdcf6a6b551432304b752d93392a897053292
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2024-06-11 08:18:20 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    [
+        'nome' => 'Anderson', 
+        'email' => 'anderson@gmail.com'
+    ],
+    [
+        'nome' => 'Maria', 
+        'email' => 'maria@gmail.com'
+    ],
+    [
+        'nome' => 'vinicius', 
+        'email' => 'vini@gmail.com'
+    ],
+    [
+        'nome' => 'Marcos', 
+        'email' => 'marcos@gmail.com'
+    ],
+    [
+        'nome' => 'Nicolas', 
+        'email' => 'Nicolas@gmail.com'
+    ],
+    [
+        'nome' => 'Romario', 
+        'email' => 'Romarinho@gmail.com'
+    ],
+    [
+        'nome' => 'Péle', 
+        'email' => 'Bomdebola@gmail.com'
+    ],
+    [
+        'nome' => 'Maradona', 
+        'email' => 'Bomdegol@gmail.com'
+    ],
+    [
+        'nome' => 'victor', 
+        'email' => 'victor@gmail.com'
+    ],
+    [
+        'nome' => 'Giovanni', 
+        'email' => 'giovanni@gmail.com'
+    ]
+
+];
---
+++ aula_api/app/status/index.php
@@ -0,0 +1,13 @@
+<?php
+    // AQUI VAI SER NOSSA APLICAÇÃO
+    require_once __DIR__ . '../../../api/config.php';
+    require_once __DIR__ . '../../../api/response.php';
+
+    echo Response::resposta(200,'success',[
+        'version' => API_VERSION,
+        'status' => 'ATIVA'
+    ]);
+
+
+
+?>
